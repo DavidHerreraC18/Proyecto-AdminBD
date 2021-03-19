552fc1e3987672f43ad786a844c5626b2099c0a4
Geraldine <avocadoenterprise@gmail.com>
2021-03-19 10:46:54 -0500

    Usuarios Espacio - Tablespaces Vistas

--- Frontend/frontend-adminbd/src/app/app-routing.module.ts
+++ Frontend/frontend-adminbd/src/app/app-routing.module.ts
@@ -5,10 +5,13 @@ import { JobsComponent } from './components/jobs/jobs.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { PermisosUsuariosComponent } from './components/permisos-usuarios/permisos-usuarios.component';
 import { TablespacesComponent } from './components/tablespaces/tablespaces.component';
+import { UsuariosDetallesComponent } from './components/usuarios-detalles/usuarios-detalles.component';
 import { UsuariosRecursosComponent } from './components/usuarios-recursos/usuarios-recursos.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
 const routes: Routes = [
+  {path: 'usuarios-detalles',
+   component: UsuariosDetallesComponent},
   {path: 'tablespaces',
    component: TablespacesComponent},
   {path: 'usuarios-recursos',
